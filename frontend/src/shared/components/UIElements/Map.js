2c7d9e95e4ffd6808c2036fe6568d6c1ecf33221
Malhar Shirswar <vhmshirswar8991@gmail.com>
2024-02-15 21:52:59 +0530

    Added Viem on Map feature to PlaceItem

---
+++ frontend/src/shared/components/UIElements/Map.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from "react";
+import Map from "ol/Map.js";
+import OSM from "ol/source/OSM.js";
+import TileLayer from "ol/layer/Tile.js";
+import View from "ol/View.js";
+
+import "ol/ol.css";
+import "./Map.scss";
+import { useGeographic } from "ol/proj";
+
+const MapView = (props) => {
+  const mapRef = useRef();
+  useGeographic();
+  const map = new Map({
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [props.center.lng, props.center.lat],
+      zoom: props.zoom,
+    }),
+  });
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  });
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
+};
+export default MapView;
