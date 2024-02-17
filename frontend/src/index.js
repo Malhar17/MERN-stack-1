6446c38787f61621d5192d4ab10ad9f50bc07e91
Malhar Shirswar <vhmshirswar8991@gmail.com>
2024-02-17 22:24:08 +0530

    Added a UpdatePlace page and fix for few minor bugs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,16 +11,20 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
-import "./index.scss";
 import UserPlaces from "./places/pages/UserPlaces";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index element={<Users />} />
-      <Route path="/:userId/places" element={<UserPlaces />} />
-      <Route path="places/new" element={<NewPlace />} />
+      <Route path=":userId/places" element={<UserPlaces />} />
+      <Route path="places">
+        <Route path="new" element={<NewPlace />} />
+        <Route path=":placeId" element={<UpdatePlace />} />
+      </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Route>
   )
